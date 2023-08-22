e010575145361cc7c259d16228360406427c991b
eliseuramos93 <eliseu.mec@gmail.com>
2023-08-22 11:49:23 -0300

    Add Comment model with validations
    Comments belongs to post, which have many comments
    
    Comments must at least have 3 characters (at least give me a LOL)

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, :body, :tags, presence: true
   validates :title, length: { minimum: 1 }
   validates :body, length: { minimum: 10 }
+
+  has_many :comments
 end
